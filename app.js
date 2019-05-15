908d0d832f99c965400189685e2efbdc16de4e0d
rodrigoss_cit <rodrigoss@ciandt.com>
2019-05-15 12:55:40 -0300

    alteracao app.js

--- app.js
+++ app.js
@@ -20,4 +20,4 @@ app.get('/',function(req,res) {
 // Console output
 app.listen(PORT, HOST);
 module.exports = app;
-console.log('Running on http://${HOST}:${PORT}'');
+console.log('Running on http://${HOST}:${PORT}');
